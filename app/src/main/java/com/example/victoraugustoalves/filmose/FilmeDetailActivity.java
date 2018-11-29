334d4fb0e12f82833ce775b409a3d839e530330f
victor alves <vtr.max@hotmail.com>
2018-11-29 16:55:09 -0200

    details page added

--- app/src/main/java/com/example/victoraugustoalves/filmose/FilmeDetailActivity.java
+++ app/src/main/java/com/example/victoraugustoalves/filmose/FilmeDetailActivity.java
@@ -1,52 +1,57 @@
 package com.example.victoraugustoalves.filmose;
 
+import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
+
+import org.w3c.dom.Text;
 
 import java.io.IOException;
 
 public class FilmeDetailActivity extends AppCompatActivity {
 
     private LinearLayout layoutImageLinearLayout;
-
+    private String img_path;
+    private ImageView imgDeatilFilme;
+    private TextView overviewTextView;
+    private TextView titleDeatailTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_filme_detail);
 
         layoutImageLinearLayout = (LinearLayout) findViewById(R.id.layoutImageLinearLayout);
+        imgDeatilFilme = (ImageView) findViewById(R.id.imgDetailImageView);
+        overviewTextView = (TextView) findViewById(R.id.overviewTextView);
+        titleDeatailTextView = (TextView) findViewById(R.id.tituloDetailTextView);
         String img_prefix = "https://image.tmdb.org/t/p/w500";
 
         Bundle b = getIntent().getExtras();
 
         String title = b.getString("title");
-        String img_path = b.getString("img_path");
+        img_path = b.getString("img_path");
         String desc = b.getString("desc");
         String date = b.getString("date");
 
-        String img_url = img_prefix + img_path;
+        img_path = img_prefix + img_path;
 
-            Bitmap bitmap = null;
-            try {
-                bitmap = Picasso.with(this).
-                load(img_url).
-                error(R.drawable.ic_dashboard_black_24dp).
-                get();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+        overviewTextView.setText(desc);
+        titleDeatailTextView.setText(title);
 
-
-        //BitmapDrawable ob = new BitmapDrawable(getResources(), bitmap);
-
-        //layoutImageLinearLayout.setBackground(ob);
-
+        Picasso.with(this.getApplicationContext()).
+                load(img_path).
+                into(imgDeatilFilme);
 
     }
+
 
 }
